872f5fe8891d5377025b393c33e873b70c6ff680
hesham <heshamelmasry1992@gmail.com>
2019-09-05 14:36:40 +0200

    write unit test

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ exports.countWordsInString = (someString) => {
   var finalResultObject = {};
 
   // make a copy of the input array
-  var copy = someString.slice(0);
+  var copy = arrayString.slice(0);
 
   // first loop goes over every element
-  for (var i = 0; i < someString.length; i++) {
+  for (var i = 0; i < arrayString.length; i++) {
 
     var myCount = 0;
     // loop over every element in the copy and see if it's the same
     for (var w = 0; w < copy.length; w++) {
-      if (someString[i] === copy[w]) {
+      if (arrayString[i] === copy[w]) {
         // increase amount of times duplicate is found
         myCount++;
         // sets item to undefined
@@ -24,14 +24,12 @@ exports.countWordsInString = (someString) => {
 
     if (myCount > 0) {
       var a = {};
-      a.value = someString[i];
+      a.value = arrayString[i];
       a.count = myCount;
       finalResultObject[a.value] = a.count;
     }
   }
-
   return finalResultObject;
 };
 
-var newArray = compressArray(testString);
-console.log(newArray);
+this.countWordsInString(testString);
--- app.test.js
+++ app.test.js
@@ -1,2 +1,5 @@
 const {countWordsInString} = require('./app');
-test('output number of words in string');
+test('output number of words in string', () => {
+  const text = countWordsInString('a b a c aa aa aa    b');
+  expect(text).toEqual({'a': 2, 'b': 2, 'c': 1, 'aa': 3});
+});
