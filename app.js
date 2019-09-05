a3f1df37ec285b5e61c5ec873895c3b97c24c167
hesham <heshamelmasry1992@gmail.com>
2019-09-05 14:46:42 +0200

    indentation and cleaning the code

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-let testString = 'a b a c aa aa aa    b';
 exports.countWordsInString = (someString) => {
-  let arrayString = [];
-  arrayString = someString.split(' ');
+  let arrayString = someString.split(' ');
   arrayString = arrayString.filter(value => Object.keys(value).length !== 0);
   var finalResultObject = {};
 
-  // make a copy of the input array
   var copy = arrayString.slice(0);
 
-  // first loop goes over every element
   for (var i = 0; i < arrayString.length; i++) {
 
     var myCount = 0;
-    // loop over every element in the copy and see if it's the same
     for (var w = 0; w < copy.length; w++) {
       if (arrayString[i] === copy[w]) {
-        // increase amount of times duplicate is found
         myCount++;
-        // sets item to undefined
         delete copy[w];
       }
     }
@@ -31,5 +24,3 @@ exports.countWordsInString = (someString) => {
   }
   return finalResultObject;
 };
-
-this.countWordsInString(testString);
